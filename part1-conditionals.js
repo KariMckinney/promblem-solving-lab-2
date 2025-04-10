6f82f2b539bcf3c1b39cf3405c58df517db01be1
KariMckinney <ajpetrikov@gmail.com>
2025-04-09 20:40:11 -0500

    JS part1

---
+++ part1-conditionals.js
@@ -0,0 +1,26 @@
+// Grade checker
+// Print A, B, C, D, or F based on score
+
+let score = 82;
+if (score) {
+    console.log("You got a B! :]");
+}
+
+// Pizza Quantity Warning
+// Print warning if quantity > 10
+
+let quantity = 12;
+if (quantity > 10){
+    console.log("Warning the quantity is over 10!")
+}
+
+
+// Login Simulator
+// Print "Access granted" or "Access denied"
+
+let password = "letmein"; // or use prompt
+if (password) {
+    console.log("Access granted");
+  } else { 
+    console.log("Access denied");
+  }
